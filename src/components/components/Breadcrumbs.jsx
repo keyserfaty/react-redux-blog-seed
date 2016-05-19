6f9e503663b21d82eccdb1c5ea4cb3b88bb8867b
Karen <karen@iamat.com>
2016-05-19 19:26:39 -0300

    add key to elements

--- src/components/components/Breadcrumbs.jsx
+++ src/components/components/Breadcrumbs.jsx
@@ -4,8 +4,8 @@ import _ from 'lodash';
 
 export const BreadCrumbs = ({ links }) => (
   <span>
-    { _.map(links, link => (
-      <span>
+    { _.map(links, (link, i) => (
+      <span key={i}>
         <Link to={link.href}>{link.name}</Link>
         <i className="fa fa-angle-right" />
       </span>
