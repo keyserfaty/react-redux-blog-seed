52298a9a20216bb4fc76e18febfdc307582eb8a7
Karen <karen@iamat.com>
2016-05-18 21:41:39 -0300

    [state] add tags

---
+++ src/state/entities/tags/actions.js
@@ -0,0 +1,54 @@
+export const TAGS_REQUEST = 'TAGS_REQUEST';
+export const TAGS_REQUEST_SUCCESS = 'TAGS_REQUEST_SUCCESS';
+export const TAGS_REQUEST_FAILURE = 'TAGS_REQUEST_FAILURE';
+
+function tagsRequest() {
+  return {
+    type: TAGS_REQUEST,
+    payload: {
+      requestingTags: true,
+      requestingTagsSuccess: false,
+    },
+  }
+};
+
+function tagsRequestSuccess(tags) {
+  return {
+    type: TAGS_REQUEST_SUCCESS,
+    payload: {
+      requestingTags: false,
+      requestingTagsSuccess: true,
+      tags,
+    },
+  }
+};
+
+function tagsRequestFailure(error) {
+  return {
+    type: TAGS_REQUEST_FAILURE,
+    payload: {
+      requestingTags: false,
+      requestingTagsSuccess: false,
+      error: new Error(error),
+    },
+  }
+};
+
+export function fetchTags() {
+  return function tagsFetch(dispatch) {
+    dispatch(tagsRequest());
+
+    return fetch('', {
+      method: 'GET',
+      credentials: 'include',
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response.status === 'true') {
+          return dispatch(tagsRequestSuccess(response));
+        }
+
+        return dispatch(tagsRequestFailure('Fetching tags failed'));
+      });
+  };
+};
--- src/state/entities/tags/reducers.js
+++ src/state/entities/tags/reducers.js
@@ -1,2 +1,32 @@
-export default tags = (state = {}, action) => {};
+import {
+  TAGS_REQUEST
+  TAGS_REQUEST_SUCCESS,
+  TAGS_REQUEST_FAILURE,
+} from './actions';
 
+const initialState = {
+  tags: {},
+  requestingTags: '',
+  requestingTagsSuccess: '',
+};
+
+export default tags = (state = initialState, action) => {
+  switch (action.type) {
+    case TAGS_REQUEST:
+    case TAGS_REQUEST_FAILURE:
+      return {
+        requestingTags: action.payload.requestingTags,
+        requestingTagsSuccess: action.payload.requestingTagsSuccess,
+      };
+
+    case TAGS_REQUEST_SUCCESS:
+      return {
+        requestingTags: action.payload.requestingTags,
+        requestingTagsSuccess: action.payload.requestingTagsSuccess,
+        tags: action.payload.tags,
+      };
+
+    default:
+      return state;
+  };
+};
